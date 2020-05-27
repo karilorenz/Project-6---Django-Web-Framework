9d44e8dd27a708b1218f338ca608931114beb16f
karilorenz <karilorenz@gmail.com>
2020-05-27 11:49:36 -1000

    Add files via upload

---
+++ DjSite/urls.py
@@ -0,0 +1,30 @@
+"""DjSite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from blog import views as blog_views
+
+
+# Rename views as import them, will have many
+# Create routes
+urlpatterns = [
+    # Single post route <pattern:variable>
+    path('post/<slug:slug>/', blog_views.post),
+    # Index page
+    path('', blog_views.index),
+    path('about/', blog_views.about),
+    path('admin/', admin.site.urls),
+]
